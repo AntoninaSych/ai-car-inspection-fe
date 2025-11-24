308ee5f743118277a519f355d83cbd7cb13e088b
Roman Onishchenko <roman.onishchenko@onelog.ch>
2025-11-24 17:59:21 +0100

    feat: add i18n

---
+++ src/i18n/index.js
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'ua',
+    debug: import.meta.env.DEV,
+    ns: ['common', 'howItWorks'],
+    defaultNS: 'common',
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
