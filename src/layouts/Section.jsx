fc94937e4403a4edac0a10fdfd678990997747ba
Roman Onishchenko <romanindev@gmail.com>
2025-12-23 15:56:31 +0100

    chore: update rivacy-policy page

--- src/layouts/Section.jsx
+++ src/layouts/Section.jsx
@@ -1,10 +1,10 @@
 import { Box } from '@mui/material';
 import { PageContainer } from './PageContainer';
 
-export const Section = ({ container = true, children, py = { xs: 6, md: 10 }, ...rest }) => {
+export const Section = ({ container = true, children, containerProps = {}, py = { xs: 6, md: 10 }, ...rest }) => {
   return (
     <Box component="section" {...rest} sx={{ py, ...rest.sx }}>
-      {container ? <PageContainer>{children}</PageContainer> : children}
+      {container ? <PageContainer {...containerProps}>{children}</PageContainer> : children}
     </Box>
   );
 };
