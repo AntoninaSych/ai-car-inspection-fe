75895ae496a9d9813182ff07d9a60247b36939fe
Roman Onishchenko <romanindev@gmail.com>
2026-01-26 15:42:16 +0100

    chore: update max size

--- src/features/upload-wizard/validation/schema.js
+++ src/features/upload-wizard/validation/schema.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const MAX_FILE_SIZE_MB = 2;
+export const MAX_FILE_SIZE_MB = 10;
 export const MAX_FILE_SIZE = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 const SUPPORTED_FORMATS = ['image/jpeg', 'image/jpg', 'image/png'];
